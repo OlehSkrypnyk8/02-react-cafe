e1bc2bccccfd7d1dc6433384a85cbed05b2c98a0
oleh skrypnyk <oleh.skrypnyk@ukr.net>
2025-11-28 21:23:46 +0200

    Add modern normalize

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,7 @@
 import css from "./App.module.css";
-import CafeInfo from "../CafeInfo/CafeInfo.tsx";
 import { useState } from "react";
+
+import CafeInfo from "../CafeInfo/CafeInfo.tsx";
 import VoteOptions from "../VoteOptions/VoteOptions.tsx";
 import VoteStats from "../VoteStats/VoteStats.tsx";
 import Notification from "../Notification/Notification.tsx";
